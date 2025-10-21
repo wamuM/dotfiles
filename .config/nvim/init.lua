15b783fc2c0adbb066a32d76d1cafcdaa8787902
wamu_M <contact@wamu-m.com>
2025-10-21 16:52:34 +0200

    Remove local clipboard to nvim
    I'm tired of pretending that it's useful to have a separated cliboard in
    vim. If I even need to protect what I have in my clipboard then I will
    use registers.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,13 @@
--- Basic Neovim settings
-vim.opt.number = true          -- Show absolute line numbers
-vim.opt.relativenumber = true  -- Show relative line numbers
+--  Long Lines
 vim.opt.wrap = false           -- Disable line wrapping
-
--- Visual guide for long lines
 vim.opt.list = true
 vim.opt.listchars:append({ extends = "»", precedes = "«" })
+
+-- Line numbers
+vim.opt.number = true          -- Show absolute line numbers
+vim.opt.relativenumber = true  -- Show relative line numbers
+
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
 
