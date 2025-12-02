d1518db194cca04bb4056b936caa143c634f353e
wamu_M <contact@wamu-m.com>
2025-12-02 23:45:55 +0100

    Add Lazy to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+require("config.lazy")
+
 --  Long Lines
 vim.opt.wrap = false           -- Disable line wrapping
 vim.opt.list = true
@@ -7,7 +9,10 @@ vim.opt.listchars:append({ extends = "»", precedes = "«" })
 vim.opt.number = true          -- Show absolute line numbers
 vim.opt.relativenumber = true  -- Show relative line numbers
 
+-- tabs
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
-
